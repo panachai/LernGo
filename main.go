1f83f85eb84e2f3643ee872ca59fb1e1e454313f
Panachai <panachai.ny@gmail.com>
2018-07-21 23:47:40 +0700

    Add main

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var age int = 40
+	var favNum float64 = 1.123123
+
+	fmt.Println(age, favNum)
+	fmt.Println("true && false = ", true && false)
+}
